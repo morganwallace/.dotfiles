197b330c00a509b054f32442a09c253b1e9ccab8
Morgan Wallace <morgan@Morgans-MacBook.hsd1.ca.comcast.net>
2018-01-05 22:54:57 -0800

    update install script with custom sh file

--- tools/install.sh
+++ tools/install.sh
@@ -80,14 +80,6 @@ main() {
   printf "${NORMAL}"
   env zsh
 
-  # update the vimrc file with the template
-  printf "${BLUE}Looking for an existing vim config...${NORMAL}\n"
-  if [ -f ~/.vimrc ] || [ -h ~/.vimrc ]; then
-    printf "${YELLOW}Found ~/.vimrc.${NORMAL} ${GREEN}Backing up to ~/.vimrc.pre-oh-my-zsh${NORMAL}\n";
-    mv ~/.vimrc ~/.vimrc.pre-oh-my-zsh;
-  fi
-  cp $ZSH/templates/.vimrc ~/
-
   # install custom plugins
   bash $ZSH/tools/install_plugins.sh
 }
